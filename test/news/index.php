f1e3203298f665956966aaef5d8dbbc39c0489b6
Alex Tretyakov <tmndnb@gmail.com>
2017-05-25 17:14:13 +0500

    test

---
+++ test/news/index.php
@@ -0,0 +1,53 @@
+<?php
+
+class View
+{
+    protected $arr;
+
+    public function assign($name, $value)
+    {
+        //- сохранить данные, передаваемые в шаблон по заданному имени (
+        //используйте защищенное свойство - массив для хранения этих данных)
+        $this->arr = [];
+        $this->arr[$name] = $value;
+        //объевили что защищенная переменная arr это пустой массив
+        //затем в этот массив добавляем ключ $name и значение $value
+
+    }
+
+    public function display($template)
+    {
+        //отображает указанный шаблон с заранее сохраненными данными
+        //прилетает ссылка на шаблон в котором будем выводить.
+        //вызывается метод чтобы выводить через буфер обмена
+        echo $this->render($template);
+
+
+    }
+
+    public function render($template)
+    {
+        ob_start();
+        include $template;
+        $content = ob_get_contents();
+        ob_end_clean();
+        return $content;
+
+    }
+}
+
+class Article
+{
+
+}
+
+class News
+{
+
+}
+
+$data = file(__DIR__.'/db.txt', FILE_IGNORE_NEW_LINES);
+foreach ($data as $datum) {
+   list ($id, $title, $news) = explode("|", $datum);
+   echo $id. ' ' .$title. ' '.$news.'<br>';
+}
