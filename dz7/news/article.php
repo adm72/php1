30fa435c06abcdf3bc94849474213930a5d3216c
Alex Tretyakov <tmndnb@gmail.com>
2017-05-30 15:26:57 +0500

    ДЗ

--- dz7/news/article.php
+++ dz7/news/article.php
@@ -4,8 +4,7 @@ require __DIR__ . '/classes/News.php';
 
 if (isset($_GET['id'])) {
 
-    $news = new News(__DIR__ . '/db.txt');
-    $data = $news->getNews();
+    $data = (new News(__DIR__ . '/db.txt'))->getId($_GET['id']);
 
     $view = new View();
     $view->assign('news', $data);
--- dz7/news/classes/News.php
+++ dz7/news/classes/News.php
@@ -5,21 +5,28 @@ require __DIR__ . '/Article.php';
 class News
 {
     public $arr;
+    public $id;
 
     public function __construct($path)
     {
         $this->path = $path;
         $this->arr = [];
-    }
-
-    public function getNews()
-    {
         $data = file($this->path, FILE_IGNORE_NEW_LINES);
 
         foreach ($data as $datum) {
             list ($id, $title, $news) = explode("|", $datum);
             $this->arr[] = new Article($id, $title, $news);
         }
+    }
+
+    public function getNews()
+    {
         return $this->arr;
+    }
+
+    public function getId($id)
+    {
+        --$id;
+        return $this->arr[$id];
     }
 }
